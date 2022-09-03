3bc0df98b1aa737b62859514400baf8c47e2f175
swc9803 <swc9803@gmail.com>
2022-09-03 18:54:21 +0900

    symbol

--- src/pages/section2.vue
+++ src/pages/section2.vue
@@ -11,6 +11,7 @@
  * new 연산자를 사용하지 않는다.
  * 심볼은 호출 시 매번 새로운 심볼 값을 생성
  * 심볼값은 문자열 형태로 출력 불가
+ * 배열 객체의 속성이 보이지 않게하려면 심볼을 써야한다.
  */
 const arr = [1, 2, 3, 4, 5]
 const length = Symbol('length')
@@ -19,4 +20,11 @@ arr[length] = 50 // arr 배열의 length 속성에 50 할당
 const symbolsame1 = Symbol('isThisSame?')
 const symbolsame2 = Symbol('isThisSame?')
 console.log(symbolsame1 === symbolsame2)
+
+const someProperty = Symbol('someProperty')
+arr[someProperty] = 10
+for (const i in arr) {
+  console.log(i)
+}
+console.log(arr[someProperty])
 </script>
