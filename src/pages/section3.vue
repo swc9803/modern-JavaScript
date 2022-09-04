dc1223193b818557414805c44dbbbf3dce108b65
swc9803 <swc9803@gmail.com>
2022-09-04 17:43:51 +0900

    prototype 개념

--- src/pages/section3.vue
+++ src/pages/section3.vue
@@ -1,8 +1,33 @@
 <template>
-    <!-- symbol -->
-    <p>{{  }}</p>
+    <!-- 프로토타입 -->
+    <p>{{ fruit }}</p>
 </template>
 
 <script setup>
+/**
+ * @프로토타입
+ * 함수 선언 후 실행 > 함수 자신, 그리고 자신과 같은 이름의 함수 '프로토타입 객체' 생성
+ */
+// const fruit = { name: 'apple' }
+// console.log(fruit.name)
+
+// fruit.expiration = 20202020
+// console.log(fruit.expiration)
+
+// const fruit2 = {
+//   name: 'apple',
+//   expiration: 20202020,
+//   greeting: () => {
+//     console.log('hi')
+//   }
+// }
+// fruit2.greeting()
+
+function Animal () {}
+const tiger = new Animal()
+const lion = new Animal()
+
+console.log(tiger)
+console.log(lion)
 
 </script>
