d9172d9e7e299bc07b7e8e96ddc6941a5050eb44
swc9803 <swc9803@gmail.com>
2022-09-06 11:57:49 +0900

    private 접근 방법

--- src/pages/section12.vue
+++ src/pages/section12.vue
@@ -1,9 +1,37 @@
 <template>
-    <!--  -->
+    <!-- private 접근 방법 -->
     <p>{{  }}</p>
 </template>
 
 <script setup>
 console.clear()
+// method
+class Animal2 {
+    #age = 4
+    bark () {
+      return `${this.#age}살 개가 짖는다.`
+    }
 
+    #privateMethod () {
+      return 'Hello, private Method'
+    }
+
+    getPrivateMethod () {
+      return this.#privateMethod()
+    }
+}
+const ani2 = new Animal2()
+console.log(ani2.getPrivateMethod())
+
+// private static method
+class Animal3 {
+  static #privateStaticMethod () {
+    return 'hello private static method'
+  }
+
+  static getPrivateStaticMethod () {
+    return this.#privateStaticMethod()
+  }
+}
+console.log(Animal3.getPrivateStaticMethod())
 </script>
