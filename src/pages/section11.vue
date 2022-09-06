46af2ee2bce5e13e5d7ce56469fc4ee8b3d10863
swc9803 <swc9803@gmail.com>
2022-09-06 11:29:49 +0900

    object.create

--- src/pages/section11.vue
+++ src/pages/section11.vue
@@ -1,9 +1,24 @@
 <template>
-    <!--  -->
+    <!-- object.create 상속 -->
     <p>{{  }}</p>
 </template>
 
 <script setup>
 console.clear()
+function ParentClass (name, age) {
+  this.name = name
+  this.age = age
+}
+ParentClass.prototype.sayHello = function () {
+  console.log(`hello ${this.name}`)
+}
+function ChildClass () {}
 
+ChildClass.prototype = Object.create(ParentClass.prototype)
+ChildClass.prototype.constructor = ChildClass
+ChildClass.prototype.move = function () {
+  console.log('moving')
+}
+const c1 = new ChildClass()
+console.log(c1)
 </script>
